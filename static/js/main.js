8a9218d541f41e9641ad3ddc5fd5c0fdd63c1b22
teaskv <teaskvarc@gmail.com>
2016-08-30 11:37:26 +0200

    pagination and link to single post/article is handled

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
-/**
- * Created by Tea on 29.8.2016.
- */
+// vseboval bi vso kodo, ki bo jo potrebovali na Front-end
+// da se zacne neki vrteti, animacija, itd.
+
+//KLIKANJE, SHARE BUTTON, etc. : te akcije ujamemo v JS, ki je v browser-ju
