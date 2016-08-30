93723218f93628a52766f01bfad124c2ba0b8914
teaskv <teaskvarc@gmail.com>
2016-08-30 11:02:18 +0200

    added new route for single article + new view: article.ejs

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app     = express();
+const request = require('request');
 
 app.set('view engine', 'ejs');
 
@@ -12,8 +13,36 @@ app.listen(3030, () =>{
 
 });
 
+/////////////////////////////////////////////////
+//ONE ARTICLE
+
+// V tem primeru je BODY string, vrne text; ni objekt; da dobimo objekt z array-i, parametri
+//moramo pognati JSON.parse
+
+app.get('/article/:id', (req, res)=>{
+
+   var articleId = req.params.id;
+
+   request('http://jsonplaceholder.typicode.com/posts/'+articleId, (err, response, body)=>{
+
+      var jsonData = JSON.parse(body);
+
+      //povemo kateri ejs rendramo, kot drugi parameter posljemo podatke
+      res.render('article', {article:jsonData});
+
+   });
+
+});
+
+
+
+
 // ce imamo articles/projects = v tem delu preden zrendramo gremo po podatke!!!
 // ker nimamo podatke uporabimo tukaj request in neke podatke
+
+
+//TA ROUTE SE POZENE OB VSAKEM KLIKU NA PAGE
+//vsakic gre na streznik po Posts na spodnji URL, jih vrne nazaj in pozene render
 
 app.get('/', (req, res)=>{
 
@@ -39,17 +68,14 @@ app.get('/', (req, res)=>{
 
          // 0 * 5 = smo na 0 strani, in vzame 5 posts.
          // zacnemo pri 0 in jih vzamemo 5 ven
-         var page = jsonData.splice(pageNum*postCount, 5);
-
-
+         var page = jsonData.splice(pageNum * postCount, 5);
 
          res.render('landing', {
-            posts    : page,
-            numPages : pageLength,
+            posts    :page,
+            numPages :pageLength,
             pageNum  :pageNum
 
          });
       }
    });
-
 });
